9c3791b18a5ed7adcddd8ef70d42f9f69cbc9f37
Camilo Arias <andres.camilo.arias@correounivalle.edu.co>
2020-03-29 14:55:13 -0500

    Se cambian datos de graficos al store de vuex

--- frontend/src/pages/Dashboard.vue
+++ frontend/src/pages/Dashboard.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="content">
     <div class="md-layout">
-      <div
-        class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-33"
-      >
+      <div  class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-33">
         <chart-card
-          :chart-data="dailySalesChart.data"
-          :chart-options="dailySalesChart.options"
+          :chart-data="usersRegisterdata.data"
+          :chart-options="usersRegisterdata.options"
           :chart-type="'Line'"
           data-background-color="blue"
         >
@@ -16,13 +14,11 @@
 
         </chart-card>
       </div>
-      <div
-        class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-33"
-      >
+      <div class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-33">
         <chart-card
-          :chart-data="emailsSubscriptionChart.data"
-          :chart-options="emailsSubscriptionChart.options"
-          :chart-responsive-options="emailsSubscriptionChart.responsiveOptions"
+          :chart-data="camerasChart.data"
+          :chart-options="camerasChart.options"
+          :chart-responsive-options="camerasChart.responsiveOptions"
           :chart-type="'Bar'"
           data-background-color="red"
         >
@@ -36,8 +32,8 @@
         class="md-layout-item md-medium-size-100 md-xsmall-size-100 md-size-33"
       >
         <chart-card
-          :chart-data="dataCompletedTasksChart.data"
-          :chart-options="dataCompletedTasksChart.options"
+          :chart-data="stationsChart.data"
+          :chart-options="stationsChart.options"
           :chart-type="'Line'"
           data-background-color="green"
         >
@@ -155,96 +151,21 @@ export default {
       totalCameras : 'totalCameras',
       totalUsers : 'totalUsers',
       totalGasStation : 'totalGasStation',
-      avgGas : 'avgGas'
+      avgGas : 'avgGas',
+      usersRegisterdata : 'usersRegisterdata',
+      stationsChart: 'stationsChart',
+      camerasChart:'camerasChart'
     })
   },
   data() {
     return {
-      dailySalesChart: {
-        data: {
-          labels: ["M", "T", "W", "T", "F", "S", "S"],
-          series: [[12, 17, 7, 17, 23, 18, 38]]
-        },
-        options: {
-          lineSmooth: this.$Chartist.Interpolation.cardinal({
-            tension: 0
-          }),
-          low: 0,
-          high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-          chartPadding: {
-            top: 0,
-            right: 0,
-            bottom: 0,
-            left: 0
-          }
-        }
-      },
-      dataCompletedTasksChart: {
-        data: {
-          labels: ["12am", "3pm", "6pm", "9pm", "12pm", "3am", "6am", "9am"],
-          series: [[230, 750, 450, 300, 280, 240, 200, 190]]
-        },
-
-        options: {
-          lineSmooth: this.$Chartist.Interpolation.cardinal({
-            tension: 0
-          }),
-          low: 0,
-          high: 1000, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-          chartPadding: {
-            top: 0,
-            right: 0,
-            bottom: 0,
-            left: 0
-          }
-        }
-      },
-      emailsSubscriptionChart: {
-        data: {
-          labels: [
-            "Ja",
-            "Fe",
-            "Ma",
-            "Ap",
-            "Mai",
-            "Ju",
-            "Jul",
-            "Au",
-            "Se",
-            "Oc",
-            "No",
-            "De"
-          ],
-          series: [[542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895]]
-        },
-        options: {
-          axisX: {
-            showGrid: false
-          },
-          low: 0,
-          high: 1000,
-          chartPadding: {
-            top: 0,
-            right: 5,
-            bottom: 0,
-            left: 0
-          }
-        },
-        responsiveOptions: [
-          [
-            "screen and (max-width: 640px)",
-            {
-              seriesBarDistance: 5,
-              axisX: {
-                labelInterpolationFnc: function(value) {
-                  return value[0];
-                }
-              }
-            }
-          ]
-        ]
-      }
     };
+  },
+  mounted(){
+    let conf = this.$Chartist.Interpolation.cardinal({tension: 0})
+    
+    this.$store.commit('setUserLineSmooth',conf);
+    this.$store.commit('setStationsLineSmooth',conf);
   }
 };
 </script>
