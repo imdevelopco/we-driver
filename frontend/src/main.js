fffdb320e875b1b0392b96dec3f405f09319b74c
jhonHenao <henao.jhon@correounivalle.edu.co>
2020-04-21 12:47:49 -0500

    retorno

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,6 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import App from "./App";
 import router from './router';
-import { VueSpinners } from '@saeris/vue-spinners'
 
 // Plugins
 import GlobalComponents from "./globalComponents";
@@ -18,11 +17,6 @@ import Chartist from "chartist";
 //state
 import { store } from './store'
 
-<<<<<<< HEAD
-=======
-
-Vue.use(VueSpinners)
->>>>>>> ad9d6baaeee4347489d340c28adcc3ba0cd85e78
 Vue.prototype.$Chartist = Chartist;
 
 Vue.use(VueRouter);
