48c83dfa9cc061c7503418ec75a3aebe176f8fac
root <root@Robot.zte.com.cn>
2020-05-09 16:00:34 -0500

    Update Users on API

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
-from .views import ListUser, RegisterUser, LoginUser
+from .views import ListUser, RegisterUser, LoginUser, UpdateUser
 from knox import views as knox_views
 
 urlpatterns = [
     path('', include('knox.urls')),
     path('registerUser', RegisterUser.as_view()),
     path('listUser', ListUser.as_view()),
-    path('login', LoginUser.as_view())
+    path('login', LoginUser.as_view()),
+    path('updateUser/<int:pk>', UpdateUser.as_view())
+    
+
 ]
 
