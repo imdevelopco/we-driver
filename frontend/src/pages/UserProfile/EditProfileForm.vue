f388de22a80325ed07d19f97099ed574ff7ff39a
imdevelopco <imdevelopco@gmail.com>
2020-05-12 16:42:58 -0500

    update User

--- frontend/src/pages/UserProfile/EditProfileForm.vue
+++ frontend/src/pages/UserProfile/EditProfileForm.vue
@@ -29,10 +29,20 @@
            <div class="md-layout-item md-small-size-100 md-size-50">
               <label>Crear items sin verificación</label>
               <br>
-              <md-input type="checkbox" >Activar</md-input>
+              <md-input type="checkbox" v-model="is_staff" >Activar</md-input>
+          </div>
+          <div class="md-layout-item md-small-size-100 md-size-50">
+              <label>Administrador</label>
+              <br>
+              <md-input type="checkbox" v-model="is_superuser">Activar</md-input>
+          </div>
+          <div class="md-layout-item md-small-size-100 md-size-50">
+              <label>Estado</label>
+              <br>
+              <md-input type="checkbox" v-model="is_active">Activo</md-input>
           </div>
           <div class="md-layout-item md-size-100 text-right">
-            <md-button class="md-raised md-success" data-background-color="blue" > Actualizar Perfil</md-button>
+            <md-button class="md-raised md-success" data-background-color="blue" @click="sendForm"> Actualizar Perfil</md-button>
           </div>
         </div>
       </md-card-content>
@@ -54,14 +64,37 @@ export default {
       email: "",
       last_name: "",
       first_name: "",
+      is_staff: false,
+      is_active: true,
+      is_superuser: false
     };
   },
   watch: {
       user(newVal) {
-          this.email = newVal.email;
-          this.last_name = newVal.last_name;
-          this.first_name = newVal.first_name;
+          this.id           =newVal.id;
+          this.email        = newVal.email;
+          this.last_name    = newVal.last_name;
+          this.first_name   = newVal.first_name;
+          this.is_staff     = newVal.is_staff;
+          this.is_active    = newVal.is_active;
+          this.is_superuser = newVal.is_superuser;
       }
+  },
+  methods:{
+    sendForm(){
+      console.log("Actualizando usuario...");
+      this.$router.push('/app/usuarios');
+      console.log(
+        this.id,
+        this.email,
+        this.last_name,
+        this.first_name,
+        this.is_staff,
+        this.is_active,
+        this.is_superuser
+      );
+      
+    }
   }
 };
 </script>
